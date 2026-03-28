a5a30ef40f45fe9cd4a661ca7862a56b7d5f0387
Almaz Khudayberdiev <super.almz2013@gmail.com>
2026-03-29 00:24:06 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,15 +3,28 @@
 // Replace with your Firebase project config
 // ==========================================
 
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkjTO4oSRdCyehUsJ1QVj0KqdB_QzigAc",
+    authDomain: "traaxcker.firebaseapp.com",
+    databaseURL: "https://traaxcker-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "traaxcker",
+    storageBucket: "traaxcker.firebasestorage.app",
+    messagingSenderId: "315863450054",
+    appId: "1:315863450054:web:9accc8bdf57fda00c6f2c7",
+    measurementId: "G-MM216YNWC3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 // Initialize Firebase
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
